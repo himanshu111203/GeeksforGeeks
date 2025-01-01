54f3cb3cda13b7a42cf771ba66665131d3fc0750
himanshu111203 <rohillahimanshu42@gmail.com>
2025-01-01 23:38:33 +0530

    Create Tower Of Hanoi (Med).cpp

---
+++ Tower Of Hanoi (Med).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    //You need to complete this function
+    void tohcal(int n,int source,int help,int dest)
+    {
+        if(n==1)
+        {
+            cout<<"move disk "<<n<<" from rod "<<source<<" to "<<dest<<endl;
+            return;
+        }
+        tohcal(n-1,source,dest,help);
+        cout<<"move disk "<<n<<" from rod "<<source<<" to "<<dest<<endl;
+        tohcal(n-1,help,source,dest);
+    }
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        tohcal(n,from,to,aux);
+        return pow(2,n)-1;
+    }
+};
