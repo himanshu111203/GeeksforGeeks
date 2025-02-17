81feddcaa91d39cbc9307f254d3ada562eb1587d
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2025-02-17 23:48:19 +0530

    Create Reverse a Stack.cpp
    Reverse a Stack
    Difficulty: MediumAccuracy: 80.5%Submissions: 99K+Points: 4Average Time: 20m
    You are given a stack St. You have to reverse the stack using recursion.
    
    Example 1:
    
    Input:
    St = {3,2,1,7,6}
    Output:
    {6,7,1,2,3}
    Explanation:
    Input stack after reversing will look like the stack in the output.
    Example 2:
    
    Input:
    St = {4,3,9,6}
    Output:
    {6,9,3,4}
    Explanation:
    Input stack after reversing will look like the stack in the output.
    Your Task:
    
    You don't need to read input or print anything. Your task is to complete the function Reverse() which takes the stack St as input and reverses the given stack.
    
    Expected Time Complexity: O(N2)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= size of the stack <= 104
    -109 <= Each element of the stack <= 109
    Sum of N over all test cases doesn't exceeds 106
    Array may contain duplicate elements.

---
+++ Stack /Medium/Reverse a Stack.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    void Reverse(stack<int> &St){
+        queue<int>q;
+        while(!St.empty())
+        {
+            q.push(St.top());
+            St.pop();
+        }
+        while(!q.empty())
+        {
+            St.push(q.front());
+            q.pop();
+        }
+    }
+};
