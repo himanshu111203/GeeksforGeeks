972d0ada83abdf86cd2d0b9c7ec492a13f384843
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-02-08 00:08:13 +0530

    Add maxProduct function for maximum product subarray
    Implement maxProduct function to find maximum product of subarray.
    Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr[].
    
    Note: It is guaranteed that the answer fits in a 32-bit integer.
    
    Examples
    
    Input: arr[] = [-2, 6, -3, -10, 0, 2]
    Output: 180
    Explanation: The subarray with maximum product is [6, -3, -10] with product = 6 * (-3) * (-10) = 180.
    Input: arr[] = [-1, -3, -10, 0, 6]
    Output: 30
    Explanation: The subarray with maximum product is [-3, -10] with product = (-3) * (-10) = 30.
    Input: arr[] = [2, 3, 4] 
    Output: 24 
    Explanation: For an array with all positive elements, the result is product of all elements. 
    Constraints:
    1 ≤ arr.size() ≤ 106
    -10  ≤  arr[i]  ≤  10

---
+++ Problem of the Day/Feb/09 Feb Maximum Product Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr) {
+        // code here
+        int prodl=INT_MIN,prodr=INT_MIN;
+        int temp1=1,temp2=1;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            temp1*=arr[i];
+            temp2*=arr[n-i-1];
+            prodl=max(prodl,temp1);
+            prodr=max(prodr,temp2);
+            if(temp1==0)
+            temp1=1;
+            if(temp2==0)
+            temp2=1;
+        }
+        return max(prodl,prodr);
+    }
+};
