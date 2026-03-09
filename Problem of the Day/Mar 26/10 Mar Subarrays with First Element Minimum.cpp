90cb8e681d9497295c4936881a16cef47163e5a6
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-03-10 01:43:37 +0530

    Update 10 Mar Subarrays with First Element Minimum.cpp
    You are given an integer array arr[ ]. Your task is to count the number of subarrays where the first element is the minimum element of that subarray.
    
    Note: A subarray is valid if its first element is not greater than any other element in that subarray.
    
    Examples:
    
    Input: arr[] = [1, 2, 1]
    Output: 5
    Explanation:
    All possible subarrays are:
    [1], [1, 2], [1, 2, 1], [2], [2, 1], [1]
    Valid subarrays are:
    [1], [1, 2], [1, 2, 1], [2], [1] -> total 5
    Input: arr[] = [1, 3, 5, 2]
    Output: 8
    Explanation:
    Valid subarrays are: [1], [1, 3], [1, 3, 5], [1, 3, 5, 2], [3], [3, 5], [5], [2] -> total 8
    Constraints:
    1 ≤ arr.size() ≤ 4*104
    1 ≤ arr[i] ≤ 105
    
    Expected Complexities
    Time Complexity: O(n)
    Auxiliary Space: O(n)

--- Problem of the Day/Mar 26/10 Mar Subarrays with First Element Minimum.cpp
+++ Problem of the Day/Mar 26/10 Mar Subarrays with First Element Minimum.cpp
@@ -1,3 +1,4 @@
+// T.C = O(N) , S.C =O(N)
 class Solution {
   public:
     int countSubarrays(vector<int> &arr) {
@@ -22,3 +23,33 @@ class Solution {
         return ans;
     }
 };
+
+// T.C = O(N) , S.C =O(1) , make the given array as next smaller element array
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        stack<int>s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[i]<arr[s.top()]){
+                arr[s.top()]=i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            arr[s.top()]=-1;
+            s.pop();
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==-1)
+            ans+=n-i;
+            else
+            ans+=arr[i]-i;
+        }
+        return ans;
+    }
+};
+
