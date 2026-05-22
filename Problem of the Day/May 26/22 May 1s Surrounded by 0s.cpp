6992bc4a8e7d9f7b316946c353bf3bb377c55c7a
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-05-22 18:35:13 +0530

    Update 22 May 1s Surrounded by 0s.cpp
    Given an n × m binary matrix grid[][], find the total count of all cells containing 1 that are unable to move out of the grid through a path of adjacent 1s.
    
    Adjacency means you can only move in four directions: Up, Down, Left, and Right. Diagonal moves are not allowed.
    Assume that the space immediately outside the grid is an open path. Any 1 located directly on the outer boundary of the grid (first row, last row, first column, or last column) can immediately step out, and any 1 connected to it can follow and also step out of the grid.
    Examples:
    
    Input: grid[][] = [[0, 0, 0, 0],
    		[1, 0, 1, 0],
    		[0, 1, 1, 0],
    		[0, 0, 0, 0]]
    Output: 3
    Explanation: The highlighted cells represent the land cells.
     
    Input: grid[][] = [[1, 1, 0, 0, 0, 1]
    		[0, 1, 1, 0, 1, 0],
    		[0, 0, 0, 1, 1, 0],
    		[0, 0, 0, 1, 1, 0],
    		[0, 1, 0, 1, 0, 0],
    		[1, 1, 0, 0, 0, 1]]
    Output: 6
    Explanation: The highlighted cells represent the land cells.
    425537429
    Constraints:
    1 ≤ n, m ≤ 500
    0 ≤ grid[i][j] ≤ 1
    
    Expected Complexities
    Time Complexity: O(n * m)
    Auxiliary Space: O(n * m)

--- Problem of the Day/May 26/22 May 1s Surrounded by 0s.cpp
+++ Problem of the Day/May 26/22 May 1s Surrounded by 0s.cpp
@@ -6,40 +6,35 @@ class Solution {
     bool valid(int i,int j){
         return i>=0 && i<n && j>=0 && j<m;
     }
-    void dfs(int i,int j,vector<vector<int>>&grid,int &c){
+    void dfs(int i,int j,vector<vector<int>>&grid){
         grid[i][j]=0;
-        c++;
         for(int k=0;k<4;k++){
             int ni=i+row[k],nj=j+col[k];
             if(valid(ni,nj) && grid[ni][nj]==1)
-            dfs(ni,nj,grid,c);
+            dfs(ni,nj,grid);
         }
     }
     int cntOnes(vector<vector<int>>& grid) {
         // code here
         n=grid.size(),m=grid[0].size();
-        int x=0;
         for(int i=0;i<n;i++){
             if(i==0 || i==n-1){
                 for(int j=0;j<m;j++){
                     if(grid[i][j]==1)
-                    dfs(i,j,grid,x);
+                    dfs(i,j,grid);
                 }
             }else{
                 if(grid[i][0]==1)
-                dfs(i,0,grid,x);
+                dfs(i,0,grid);
                 if(grid[i][m-1]==1)
-                dfs(i,m-1,grid,x);
+                dfs(i,m-1,grid);
             }
         }
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    int c=0;
-                    dfs(i,j,grid,c);
-                    ans+=c;;
-                }
+                if(grid[i][j]==1)
+                ans++;
             }
         }
         return ans;
