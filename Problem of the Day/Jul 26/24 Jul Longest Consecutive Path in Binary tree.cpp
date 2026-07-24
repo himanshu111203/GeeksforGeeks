b6eda5ca5ca00913971934fe6d7e27e21fc3d18f
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-07-24 22:14:06 +0530

    Create 24 Jul Longest Consecutive Path in Binary tree.cpp
    Given the root of a Binary Tree, find the length of the longest path consisting of connected nodes such that each next node has a value exactly 1 greater than its parent.
    
    The path must move from parent to child only and follow increasing consecutive values.
    
    If no such path exists, return -1.
    
    Examples:
    
    Input: root[] = [1, 2, 3]
    
                                    
    Output: 2
    Explanation : Longest sequence is 1, 2. So answer for this test case is 2.
    Input : root[] = [10, 20, 30, 40, N, 60, 90]
    
    Output : -1
    Explanation: For the above test case no sequence is possible. So output is -1.
    
    Constraints:
    
    1 ≤ no. of nodes in root ≤ 105
    1 ≤ root.node->data ≤ 105
    Expected Complexities
    Time Complexity: O(n)
    Auxiliary Space: O(Height of the tree)

---
+++ Problem of the Day/Jul 26/24 Jul Longest Consecutive Path in Binary tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int ans=1;
+    void dfs(Node *root,int len){
+        if(!root)
+        return;
+        ans=max(ans,len);
+        if(root->left){
+            if(root->left->data==root->data+1)
+            dfs(root->left,len+1);
+            else
+            dfs(root->left,1);
+        }
+        if(root->right){
+            if(root->right->data==root->data+1)
+            dfs(root->right,len+1);
+            else
+            dfs(root->right,1);
+        }
+    }
+    int longestConsecutive(Node* root) {
+        // code here
+        if(!root)
+        return -1;
+        dfs(root,1);
+        return (ans==1)?-1:ans;
+    }
+};
